fa3883748f03aece84b82cc479373023ea96018f
jundaf2 <jundafeng1998@gmail.com>
2023-09-07 21:37:13 +0800

    save

--- fused_two_gemms_f16_sm80.cu
+++ fused_two_gemms_f16_sm80.cu
@@ -199,6 +199,8 @@ bool run_fused_gemm_f16_sm80_rf_res(cutlass::gemm::GemmCoord gemm_param0, cutlas
   ElementCompute alpha1 = ElementCompute(1);
   ElementCompute beta1 = ElementCompute(1); //beta=1 for bias
 
+  // constexpr int GEMM0_BLK_WRP_N = 256;
+  // constexpr int GEMM1_BLK_WRP_N = 128;
   using ThreadblockShape0 = cutlass::gemm::GemmShape<64, 64, 32>;
   using WarpShape0 = cutlass::gemm::GemmShape<16, 64, 32>;
   using ThreadblockShape1 = cutlass::gemm::GemmShape<64, 128, 32>;
@@ -257,15 +259,92 @@ bool run_fused_gemm_f16_sm80_rf_res(cutlass::gemm::GemmCoord gemm_param0, cutlas
 
 }
 
+
+bool run_fused_gemm_f16_sm80_shmem(cutlass::gemm::GemmCoord gemm_param0, cutlass::gemm::GemmCoord gemm_param1) {
+
+  using ElementOutput = cutlass::half_t;
+  using ElementAccumulator = cutlass::half_t;
+  using ElementCompute = cutlass::half_t;
+
+  ElementCompute alpha0 = ElementCompute(1);
+  //Fused kernel has built-in bias, setting beta=0
+  ElementCompute beta0 = ElementCompute(0); 
+  ElementCompute alpha1 = ElementCompute(1);
+  ElementCompute beta1 = ElementCompute(1); //beta=1 for bias
+
+  // 240 2560 5120
+  using ThreadblockShape0 = cutlass::gemm::GemmShape<64, 256, 32>;
+  using WarpShape0 = cutlass::gemm::GemmShape<32, 32, 32>;
+  using ThreadblockShape1 = cutlass::gemm::GemmShape<64, 512, 32>;
+  using WarpShape1 = cutlass::gemm::GemmShape<64, 64, 32>;
+  using InstructionShape = cutlass::gemm::GemmShape<16, 8, 16>;
+
+  using EpilogueOutputOp0 = 
+    cutlass::epilogue::thread::LinearCombinationRelu<
+      ElementOutput,
+      InstructionShape::kM * InstructionShape::kN / 32,
+      ElementAccumulator,
+      ElementCompute,
+      cutlass::epilogue::thread::ScaleType::OnlyAlphaScaling
+    >;
+
+  using EpilogueOutputOp1 = 
+    cutlass::epilogue::thread::LinearCombinationRelu<
+      ElementOutput,
+      128 / cutlass::sizeof_bits<ElementOutput>::value,
+      ElementAccumulator,
+      ElementCompute,
+      cutlass::epilogue::thread::ScaleType::NoBetaScaling
+    >;
+
+
+  const bool SmemAccumulator = true;
+
+  using B2bGemm = cutlass::gemm::device::B2bGemm<
+    cutlass::half_t,
+    cutlass::layout::RowMajor,
+    cutlass::half_t,
+    cutlass::layout::ColumnMajor,
+    ElementOutput,
+    cutlass::layout::RowMajor,
+    ElementAccumulator,
+    cutlass::arch::OpClassTensorOp,
+    cutlass::arch::Sm80,
+    ThreadblockShape0,
+    ThreadblockShape1,
+    WarpShape0,
+    WarpShape1,
+    InstructionShape,
+    EpilogueOutputOp0,
+    EpilogueOutputOp1,
+    cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<1>,
+    3,
+    SmemAccumulator
+  >;
+
+  B2bFusedGemmRun<B2bGemm> fusedGemm;
+
+  std::cout << "Running Fused back-to-back FP16 TN GEMMs with shared memory staging...\n";
+  bool passed = fusedGemm.run(gemm_param0, gemm_param1, alpha0, beta0, alpha1, beta1);
+  if(passed)
+    std::cout << "Pass\n";
+  else
+    std::cout << "Fail\n";
+
+  return passed;
+
+}
+
 int main(int argc, char * const argv[]) {
   int batch = std::atoi(argv[1]);
-  int input_dim = std::atoi(argv[2]);
-  int seq_len = std::atoi(argv[3]);
-  int tp_degree = std::atoi(argv[4]);
+  int input_dim = std::atoi(argv[2]); // K
+  int seq_len = std::atoi(argv[3]); // M
+  int tp_degree = std::atoi(argv[4]); 
   int expert_count = std::atoi(argv[5]);
-  int expert_dim = std::atoi(argv[6]);
+  int expert_dim = std::atoi(argv[6]); // N
   std::cout << "batch: " << batch << ", input_dim: " << input_dim << ", seq_len: " << seq_len << ", tp_degree: " << tp_degree << ", expert_count: " << expert_count << ", expert_dim: " << expert_dim << std::endl;
 
+  // M N K: (0) M N K (1) M K N
   int M = batch * seq_len / expert_count;
   int K = input_dim;
   int N = expert_dim / tp_degree;
@@ -274,13 +353,20 @@ int main(int argc, char * const argv[]) {
   cutlass::gemm::GemmCoord gemm_f16_sm80_problem_size_0(M, K, N);
   cutlass::gemm::GemmCoord gemm_f16_sm80_problem_size_1(M, N, K);
 
+  // cutlass::gemm::GemmCoord gemm_f16_sm80_problem_size_0(128*640, 64, 576);
+  // cutlass::gemm::GemmCoord gemm_f16_sm80_problem_size_1(128*640, 128, 64);
+  // cutlass::gemm::GemmCoord gemm_f16_sm80_problem_size_0(128*640, 64, 576);
+  // cutlass::gemm::GemmCoord gemm_f16_sm80_problem_size_1(128*640, 256, 64);
+
   std::vector<bool (*)(cutlass::gemm::GemmCoord, cutlass::gemm::GemmCoord)>funcs = {
     &run_nonfused_gemm_f16_sm80
     ,
     &run_fused_gemm_f16_sm80_rf_res
+    ,
+    &run_fused_gemm_f16_sm80_shmem
   };
 
-  return testRun(80, funcs, gemm_f16_sm80_problem_size_0, gemm_f16_sm80_problem_size_1, "gemm f16 RF residency");
+  return testRun(80, funcs, gemm_f16_sm80_problem_size_0, gemm_f16_sm80_problem_size_1, "gemm f16");
 
 }
 
